ad57b8e00ee7d7fe71c44eb64b648a158f05fff6
joel-wenzel <joel.wenzel@flexagon.com>
2023-07-19 11:20:50 -0500

    first commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  // hitting ctrl space on HelloWorld to import results in a bad import path
+</script>
+
+<template>
+  <div>
+    <HelloWorld></HelloWorld>
+  </div>
+</template>
---
+++ pages/hello/HelloWorld.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+  const msg = ref('Hello')
+</script>
+<template>
+  <div>{{ msg }}</div>
+</template>
